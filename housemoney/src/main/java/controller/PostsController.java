f4dbfc1964233233b42da2ec335e546a431e8e01
mimura15 <5101south@gmail.com>
2023-04-19 14:41:23 +0900

    remove cash

---
+++ housemoney/src/main/java/controller/PostsController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+public class PostsController {
+    
+    @Autowired
+    private GetAllPostsService getAllPostsService;
+    
+    //一覧ページ取得
+    @GetMapping
+    public String index(Model model) {
+        List<Post> posts = getAllPostsService.getAllPosts();
+        model.addAttribute("posts", posts);
+    }
+    //投稿
+    @PostMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("form", new CreatePostForm());
+        return "/posts/create";
+    }
+}
