2cccfe9ff1144401f1059ca520a56ffbe5610d70
yasunakayayoi <yasunaka.yayoi@alhinc.jp>
2023-04-19 18:01:22 +0900

    [add]

--- housemoney/src/main/java/service/CreateUserService.java
+++ housemoney/src/main/java/service/CreateUserService.java
@@ -1,27 +1,30 @@
 package service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import entity.User;
-import form.LoginUserForm;
+import form.CreateUserForm;
 import mapper.UserMapper;
 
 @Service
-public class LoginUserService {
+public class CreateUserService {
 
 	@Autowired
 	private UserMapper mapper;
 	
 	@Transactional
-	public User login(LoginUserForm form) {
+	public int create(CreateUserForm form) {
 		User entity = new User();
 		entity.setName(form.getName());
+		entity.setPhoneNumber(form.getPhoneNumber());
 		entity.setPassword(form.getPassword());
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		entity.setPassword(encoder.encode(form.getPassword()));
 		
-		return mapper.login(entity);
-		
+		return mapper.create(entity);
 	}
+
 }
